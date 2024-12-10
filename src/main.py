8c8aef249894b170eb784766867f587e06c7643d
mtkumar123 <mtkumar@ncsu.edu>
2024-12-10 21:46:13 +0200

    update description

--- src/main.py
+++ src/main.py
@@ -4,7 +4,12 @@ from src.routers.upload.router import router as upload_router
 from src.routers.validation.router import router as validation_router
 from src.utils import lifespan
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    lifespan=lifespan,
+    title="Schema Inference and Validation",
+    description="Inference and validation of schemas against csv files.",
+    version="1.0.0",
+)
 
 app.include_router(schema_router)
 app.include_router(upload_router)
