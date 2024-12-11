12f2cf9766f8239582d0f7b96f2e72776a0293c3
mtkumar123 <mtkumar@ncsu.edu>
2024-12-11 22:10:20 +0200

    handle same file being uploaded twice

--- src/routers/upload/crud.py
+++ src/routers/upload/crud.py
@@ -5,4 +5,9 @@ collection_name = "files"
 
 
 def create_file(session: Database, file_model: FileDBModel):
-    session[collection_name].insert_one(file_model.model_dump())
+    if session[collection_name].find_one({"file_name": file_model.file_name}):
+        session[collection_name].replace_one(
+            {"file_name": file_model.file_name}, file_model.model_dump()
+        )
+    else:
+        session[collection_name].insert_one(file_model.model_dump())
